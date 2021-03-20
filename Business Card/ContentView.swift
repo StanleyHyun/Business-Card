0ddf1c0188bc9778f89af4556d3fad592f6ac9db
stanley hyun <stanleyh.nextacademy@gmail.com>
2021-03-20 08:32:57 -0400

    Initial Commit

---
+++ Business Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Business Card
+//
+//  Created by Stanley Hyun on 3/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
